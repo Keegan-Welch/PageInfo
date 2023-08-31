88d8febe2395c650864abf110f0d5d2702a6d082
K Welch <114922650+Keegan-Welch@users.noreply.github.com>
2023-08-31 01:35:10 -0400

    finished project

--- Main.py
+++ Main.py
@@ -1,6 +1,29 @@
-from pyo import *
-s = Server().boot()
-s.amp = 1
-a = Sine().out()   # Plays on left channel
-a2 = Sine().out(1) # Plays on right channel
-s.gui(locals())
+import os
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+
+
+userInput = input("URL (eg: google.com): ")
+pageUrl = "https://www." + userInput
+pageFileName = userInput + ".html"
+
+page = requests.get(pageUrl)
+pageLinks = page.links
+pageEncoding = page.encoding
+pageContent = page.content.decode(pageEncoding)
+
+
+pageInfoList = [pageUrl, page, pageEncoding, pageContent]
+os.system("cls")
+
+pageHTML = open(pageFileName, "w")
+for line in pageContent:
+    pageHTML.write(line)
+
+
+for i in pageInfoList:
+    if i == pageContent:
+        print(f"page html logged in {pageFileName}")
+    else:
+        print(i)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# PythonStuff
- 
+# PageInfo
+scrapes some of the information from an inputted url.
